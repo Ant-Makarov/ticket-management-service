508c947cacf17bea5c618f4226d6a668ac02f435
Anton Makarov <ant.makarov1997@gmail.com>
2023-04-14 15:56:02 +0300

    commit

---
+++ src/main/java/com/ftb/ticketmanagementservice/TicketManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ftb.ticketmanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketManagementServiceApplication.class, args);
+	}
+
+}
